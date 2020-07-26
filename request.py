91f47000cb314df75709a791770f798ddcf202a3
syedareehaquasar <reeha.saba@gmail.com>
2020-07-27 04:39:25 +0530

    Sentimental analysis project uploaded

---
+++ request.py
@@ -0,0 +1,7 @@
+
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'SeniorCitizen':0,'Tenure':1, 'MonthlyCharges':29.85, 'TotalCharges':29.85})
+
+print(r.json())
